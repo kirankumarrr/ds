df24ca4d3624224c592809e095fda1f8cf90fb52
kirankumarrr <kirankumarrr>
2020-04-20 23:11:39 +0530

    BST depthFirstTraversal, inOrder,Pre Order

--- Strutures/BinarySearchTree.js
+++ Strutures/BinarySearchTree.js
@@ -22,6 +22,34 @@ BST.prototype.contains = function(searchValue){
         return this.right ? this.right.contains(searchValue) : false;
     }
 }
+
+//Depth First Traversal
+BST.prototype.depthFirstTraversal = function(iteratorFxn){
+    //Running this iterator in Order
+    this.left ? this.left.depthFirstTraversal(iteratorFxn) : null
+    iteratorFxn(this.value);
+    this.right? this.right.depthFirstTraversal(iteratorFxn):  null
+}
+
+//Depth First Traversal Order
+BST.prototype.depthFirstTraversalOrder = function(iteratorFxn,order){
+    //Running this iterator pre-order will while cloning BST
+    order==='pre-order'? iteratorFxn(this.value) : null
+    this.left ? this.left.depthFirstTraversalOrder(iteratorFxn,order) : null
+    order==='in-order'? iteratorFxn(this.value) : null
+    this.right? this.right.depthFirstTraversalOrder(iteratorFxn,order):  null
+}
+
 var bst = new BST(50);
 bst.insert(20);
+bst.insert(15);
+bst.insert(25);
 bst.contains(20)
+function log(value){console.log(value)};
+
+console.log('%c depthFirstTraversalOrder in-order! ', 'background: #222; color: #bada55');
+bst.depthFirstTraversalOrder(log,'in-order');
+console.log('%c depthFirstTraversalOrder pre-order! ', 'background: #222; color: #bada55');
+bst.depthFirstTraversalOrder(log,'pre-order');
+console.log('%c depthFirstTraversal! ', 'background: #222; color: #bada55');
+bst.depthFirstTraversal(log);
